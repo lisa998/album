a0aa443b9d1da71b8fc91dca16fe92f9b9cce9f9
lisa <xj3zjfan@gmail.com>
2021-11-22 15:22:28 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./component/home/home";
+import { Routes, Route } from "react-router-dom";
+import Slider from "./component/slider/slider";
+import { selectPic } from "./component/home/picSlice";
+import { useSelector } from "react-redux";
+import { loadAllPic, addAlbum } from "./component/home/picSlice";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
 
 function App() {
+  const pic = useSelector(selectPic);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(loadAllPic()).then((r) =>
+      r.payload.map((e) => dispatch(addAlbum({ name: e.name, img: e.src })))
+    );
+  }, [dispatch]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {Object.keys(pic).map((ele) => (
+          <Route
+            path={`/${encodeURIComponent(ele)}`}
+            element={<Slider name={ele} />}
+            key={ele}
+          />
+        ))}
+      </Routes>
     </div>
   );
 }
