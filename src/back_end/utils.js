dc48ea6322bad05b45dff2f16a1b3a228bffecba
lisa <xj3zjfan@gmail.com>
2021-11-27 23:21:05 +0800

    change comp image percentage

--- src/back_end/utils.js
+++ src/back_end/utils.js
@@ -3,14 +3,19 @@ const fs = require("fs");
 const path = require("path");
 const { handleImg } = require("./conn");
 
-const compImg = async (file, options, src) => {
-  try {
-    const thumbnail = await imageThumbnail(file, options);
-    let small =
+const compImg = async (file, percentage, src) => {
+  let small = "";
+  if (percentage === 25) {
+    small = path.join(__dirname, "../../static") + "/" + src;
+  } else {
+    small =
       path.join(__dirname, "../../static") +
       "/" +
       src.split(".jpg")[0] +
       "_small.jpg";
+  }
+  try {
+    const thumbnail = await imageThumbnail(file, { percentage });
     const fd = await fs.openSync(small, "w");
     await fs.writeSync(fd, thumbnail);
     await fs.closeSync(fd);
@@ -30,13 +35,8 @@ const deleteImg = async (img) => {
 
 const uploadFile = async (name, file) => {
   const src = path.basename(file.filepath);
-  await compImg(
-    file.filepath,
-    {
-      percentage: 25,
-    },
-    src
-  );
+  await compImg(file.filepath, 25, src);
+  await compImg(file.filepath, 10, src);
   await handleImg.upload({ name, src });
 };
 
