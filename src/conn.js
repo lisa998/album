51ad8023dd4d35b3642c8f86ff8ca6af984dd10d
lisa <xj3zjfan@gmail.com>
2021-11-27 17:45:18 +0800

    add env var name with REACT_APP_

--- src/conn.js
+++ src/conn.js
@@ -1,4 +1,4 @@
 export const getApiUrl = (path) =>
-  process.env["API_URL"]
-    ? `${process.env["API_URL"]}/${path}`
+  process.env["REACT_APP_API_URL"]
+    ? `${process.env["REACT_APP_API_URL"]}/${path}`
     : `http://localhost:3001/${path}`;
