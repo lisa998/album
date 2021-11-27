cf415aa39027d7290b96f0f42485fc4443939d29
lisa <xj3zjfan@gmail.com>
2021-11-27 16:42:24 +0800

    fix sql env bug

--- src/back_end/conn.js
+++ src/back_end/conn.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 const { Sequelize, DataTypes } = require("sequelize");
 const sequelize = new Sequelize(
+  process.env["DB_NAME"],
   process.env["DB_USER"],
   process.env["DB_PASS"],
-  process.env["DB_NAME"],
   {
     host: process.env["DB_HOST"],
     dialect: "mysql",
